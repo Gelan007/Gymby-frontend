bc52d67f77cc6b91a2d15a73e95550ad03b37489
Gelan007 <anatoliy.fil033@gmail.com>
2023-04-18 00:40:17 +0300

    hover для AddingCardPhotos

--- gymby-main-frontend-app/src/components/general/addingCardPhotos/PhotoCard.jsx
+++ gymby-main-frontend-app/src/components/general/addingCardPhotos/PhotoCard.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import s from './AddingCardPhotos.module.scss'
 import zagluscha from '../../../assets/images/userCabinet/personalData/Revan.jpg'
 import basketIcon from '../../../assets/images/general/icons/basketWhiteBackground.svg'
 
 const PhotoCard = () => {
+
     return (
         <div className={s.photoCard}>
-            {/*карточка с фоткой и уронй справа сверху*/}
+            {/*карточка с фоткой и урной справа сверху*/}
             <img src={zagluscha} alt="user photo"/>
             <div className={s.basketIcon}>
                 <img src={basketIcon} alt="basket"/>
