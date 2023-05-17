6abfa8bf4a5eba7ea26b2cb9b826aeb2ff6be4f4
Gelan007 <anatoliy.fil033@gmail.com>
2023-05-17 15:47:51 +0300

    ProgramsCard

--- gymby-main-frontend-app/src/components/programs/card/ProgramsCard.jsx
+++ gymby-main-frontend-app/src/components/programs/card/ProgramsCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import s from './ProgramsCard.module.scss'
 
 const ProgramsCard = () => {
     return (
-        <div>
-
+        <div className={s.card}>
+            <div className={s.card__body}>
+                <div className={s.card__text}>
+                    4-недільна програма на масу від Івана
+                </div>
+                <div className={s.marksBlock}>
+                    <span>ектомороф</span>
+                    <span>набір маси sdfd</span>
+                    <span>набір</span>
+                    <span>набір мас</span>
+                    <span>набір маси sdfd</span>
+                    <span>набір маси sdfd</span>
+                </div>
+            </div>
         </div>
     );
 };
