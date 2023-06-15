b0b622143d287bbe405e586f2cd0275f777e00c1
Gelan007 <anatoliy.fil033@gmail.com>
2023-06-15 23:55:01 +0300

    Добавлено отображение значка после добавления в друзья

--- gymby-main-frontend-app/src/components/userAccount/instruction/UserAccountInstruction.jsx
+++ gymby-main-frontend-app/src/components/userAccount/instruction/UserAccountInstruction.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import UserAccountLeftPanel from "../leftPanel/UserAccountLeftPanel";
 import s from "./Instruction.module.scss";
 import UserAccountLeftPanelContainer from "../leftPanel/UserAccountLeftPanelContainer";
+import notebook from "../../../assets/images/LandingPage/mainLandingPage/Macbook.svg";
 
 const UserAccountInstruction = () => {
     return (
         <div className={s.instruction}>
             <UserAccountLeftPanelContainer isButtonGold={true}/>
             <div className={s.instruction__body}>
-                Instruction block
+                <div className={s.video}>
+                    <div className={s.iframeBlock}>
+                        <iframe src="https://www.youtube.com/embed/8Jugz5prO7g"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowFullScreen></iframe>
+                    </div>
+                    <img src={notebook} alt="notebook"/>
+                </div>
             </div>
         </div>
     );
--- gymby-main-frontend-app/src/components/userAccount/search/UserAccountSearchItem.jsx
+++ gymby-main-frontend-app/src/components/userAccount/search/UserAccountSearchItem.jsx
@@ -1,14 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './SearchItem.module.scss'
 import defaultPhoto from '../../../assets/images/userCabinet/personalData/profile.png'
 import trainerIcon from '../../../assets/images/general/icons/trainer_icon.svg'
 import addUserIcon from '../../../assets/images/general/icons/addUser_icon.svg'
+import verified from '../../../assets/images/userCabinet/search/verified-user.png'
 
 
 const UserAccountSearchItem = ({username, firstName, lastName, isCoach, inviteFriend, avatar}) => {
+    const [isInviteSent, setIsInviteSend] = useState(false)
     const handleAddFriend = (e) => {
         e.preventDefault()
         inviteFriend(username)
+        setIsInviteSend(true)
     }
 
     return (
@@ -30,9 +33,16 @@ const UserAccountSearchItem = ({username, firstName, lastName, isCoach, inviteFr
                         <div></div>
                     }
                 </div>
-                <div className={s.addIcon} onClick={(e) => handleAddFriend(e)}>
+                {isInviteSent ?
+                    <div className={s.verifiedIcon}>
+                        <img src={verified} alt="addUserIcon"/>
+                    </div>
+                    :
+                    <div className={s.addIcon} onClick={(e) => handleAddFriend(e)}>
                     <img src={addUserIcon} alt="addUserIcon"/>
-                </div>
+                    </div>
+                }
+
             </div>
         </div>
     );
