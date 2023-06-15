643d91f40c6c81dcef28bb527b4a060228b3c2df
Gelan007 <anatoliy.fil033@gmail.com>
2023-06-15 14:42:39 +0300

    Выбор даты в добавлении фоток в замерах

--- gymby-main-frontend-app/src/components/measurements/photos/MeasurementPhotos.jsx
+++ gymby-main-frontend-app/src/components/measurements/photos/MeasurementPhotos.jsx
@@ -5,7 +5,8 @@ import ButtonGreen from "../../UI/buttons/ButtonGreen";
 import {useTranslation} from "react-i18next";
 import MeasurementsPhotoItem from "../photoItem/MeasurementsPhotoItem";
 
-const MeasurementPhotos = ({photos, handleAddButton, addMeasurementPhotoCreationDate, handleDeleteButton}) => {
+const MeasurementPhotos = ({photos, handleAddButton, addMeasurementPhotoCreationDate, handleDeleteButton,
+                               dateToForbidSelectFutureDates, userDateData, handleInputDateChange}) => {
     const {t} = useTranslation()
 
    return (
@@ -14,8 +15,15 @@ const MeasurementPhotos = ({photos, handleAddButton, addMeasurementPhotoCreation
            <div className={s.measurementsPhotoItem__body}>
                <div className={s.navBlock}>
                    <div className={s.navBlock__title}>{t("measurements.leftPanel.photos")}</div>
-                   <div className={s.navBlock__button}>
-                       <ButtonGreen onClick={(() => addMeasurementPhotoCreationDate())}>{t("measurements.add")}</ButtonGreen>
+                   <div className={s.navBlock__rightBlock}>
+                       <div className={s.navBlock__date}>
+                           <input type="date" max={dateToForbidSelectFutureDates}
+                                  value={userDateData.formattedDate}
+                                  onChange={(e) => handleInputDateChange(e)}/>
+                       </div>
+                       <div className={s.navBlock__button}>
+                           <ButtonGreen onClick={(() => addMeasurementPhotoCreationDate(userDateData.date))}>{t("measurements.add")}</ButtonGreen>
+                       </div>
                    </div>
                </div>
                <div className={s.photosList}>
--- gymby-main-frontend-app/src/components/measurements/photos/MeasurementPhotosContainer.jsx
+++ gymby-main-frontend-app/src/components/measurements/photos/MeasurementPhotosContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import MeasurementPhotos from "./MeasurementPhotos";
 import {
     addMeasurementPhoto,
@@ -8,23 +8,48 @@ import {
 } from "../../../redux/slices/measurements-slice";
 import {connect} from "react-redux";
 
+
 const MeasurementPhotosContainer = (props) => {
-    const handleAddButton = (e) => {
-        const file = e.target.files[0];
-        props.addMeasurementPhoto({photo: file})
-    }
-    const handleDeleteButton = (photoData) => {
-        const {id} = photoData
-        props.deleteMeasurementPhoto({photoId: id})
-    }
+
+    const [dateToForbidSelectFutureDates, setDateToForbidSelectFutureDates] = useState('')
+    const [userDateData, setUserDateData] = useState({})
+
+    useEffect(() => {
+        const dateNow = new Date()
+        dateNow.setDate(dateNow.getDate() + 1)
+        setDateToForbidSelectFutureDates(dateNow.toISOString().split('T')[0])
+    }, [])
+
+    useEffect(() => {
+        let dateWithExtraHoursForCorrectDisplaying = new Date();
+        dateWithExtraHoursForCorrectDisplaying.setTime(dateWithExtraHoursForCorrectDisplaying.getTime() + 10 * 18 * 60 * 1000)
+        const isoDate = dateWithExtraHoursForCorrectDisplaying.toISOString()
+        setUserDateData({date: isoDate, formattedDate: isoDate.slice(0, 10)})
+    }, [])
 
     useEffect(() => {
         props.getMeasurements()
     }, [])
 
+    const handleAddButton = (e) => {
+        const file = e.target.files[0];
+        props.addMeasurementPhoto({photo: file, date: userDateData.date})
+    }
+    const handleDeleteButton = (photoData) => {
+        const {id} = photoData
+        props.deleteMeasurementPhoto({photoId: id})
+    }
+    const handleInputDateChange = (event) => {
+        const newDate = new Date(event.target.value)
+        newDate.setTime(newDate.getTime() + 10 * 18 * 60 * 1000)
+        const isoDate = newDate.toISOString();
+        setUserDateData({...userDateData, date: isoDate, formattedDate: event.target.value});
+    };
 
     return (
-        <MeasurementPhotos handleAddButton={handleAddButton} handleDeleteButton={handleDeleteButton} photos={props.photos} {...props}/>
+        <MeasurementPhotos handleAddButton={handleAddButton} handleDeleteButton={handleDeleteButton}
+                           photos={props.photos} dateToForbidSelectFutureDates={dateToForbidSelectFutureDates}
+                           userDateData={userDateData} handleInputDateChange={handleInputDateChange} {...props}/>
     );
 };
 
